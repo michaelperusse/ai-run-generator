780edbe8d98245efc8f5cd755d99ae1da2f755ca
Michael Perusse <michaelperusse@Michaels-MBP-2.localdomain>
2024-01-01 21:30:55 -0800

    updated paces

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react';
 import '../src/app/globals.css';
 import Replicate from "replicate";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
@@ -64,6 +64,8 @@ const Home = () => {
 
       userImage.onload = () => {
         userImageLoaded = true;
+        canvas.width = userImage.naturalWidth;
+        canvas.height = userImage.naturalHeight;
         drawImages();
       };
 
@@ -112,33 +114,6 @@ const Home = () => {
 
       }
 
-      // userImage.onload = () => {
-      //   ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
-      //   ctx.font = '16px Arial';
-
-      //   const canvasWidth = canvas.width;
-
-      //   const textTitlePosY = canvas.height - canvas.height / 10;
-      //   const subtitlePosY = textTitlePosY + canvas.height / 22;
-
-      //   ctx.font = '18px Tahoma';
-      //   ctx.fillStyle = 'white';
-      //   ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
-      //   ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
-      //   ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
-
-      //   ctx.font = '30px Tahoma';
-      //   ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth  / 3) * 0 + (canvasWidth / 10), subtitlePosY);
-      //   ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
-      //   ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
-
-      //   stravaLogo.onload = () => {
-      //     const stravaRatio = 4.76;
-      //     const stravaWidth = 100;
-      //     const stravaHeight = stravaWidth / stravaRatio;
-      //     ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
-      //   };
-      // };
     } catch (error) {
       console.error('Error creating prediction:', error);
     }
@@ -146,9 +121,9 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full pt-20">
-      <div className="w-full max-w-md mx-auto border py-8 px-0 md:p-12 rounded-xl shadow-sm mx-8">
+      <div className="w-full max-w-md md:mx-auto border py-8 px-8 md:p-12 rounded-xl shadow-sm">
         <h1 className="text-4xl text-gray-800 font-bold mb-2 text-left">AI Run Generator 🏃‍♀️</h1>
-        <div className='text-xs text-gray-600 mb-2'>Let our GPUs run for you. Create awesome Strava posts without the hassle of actually running.</div>
+        <div className='text-xs text-gray-600 mb-2'>Let our GPUs run for you. Create awesome Strava parody posts without the hassle of actually running.</div>
         <div className='w-7/8 flex mx-auto align-center items-center border-b border-gray-200 mb-6'></div>
 
         <form onSubmit={handleSubmit} className="flex flex-col items-left space-y-6 mx-auto justify-center">
@@ -188,41 +163,62 @@ const Home = () => {
               value={pace}
               onChange={(e) => {
                 const selectedSpeed = e.target.value;
-                let calculatedPace, calculatedTime;
-
                 // Do all calculations in miles, update pace at the end if in km
+
+                let basePace; // in minutes per mile or kilometer
 
                 switch (selectedSpeed) {
                   case 'superslow':
-                    calculatedPace = '10:00'; // replace with your logic
-                    calculatedTime = '60:00'; // replace with your logic
+                    basePace = 30;
                     break;
                   case 'slow':
-                    calculatedPace = '10:00'; // replace with your logic
-                    calculatedTime = '60:00'; // replace with your logic
+                    basePace = 20;
                     break;
                   case 'respectable':
-                    calculatedPace = '10:00'; // replace with your logic
-                    calculatedTime = '60:00'; // replace with your logic
+                    basePace = 10;
                     break;
                   case 'fast':
-                    calculatedPace = '7:30'; // replace with your logic
-                    calculatedTime = '45:00'; // replace with your logic
+                    basePace = 7;
                     break;
                   case 'wickedfast':
-                    calculatedPace = '5:00'; // replace with your logic
-                    calculatedTime = '30:00'; // replace with your logic
+                    basePace = 5;
                     break;
                   case 'unbelievablyfast':
-                    calculatedPace = '5:00'; // replace with your logic
-                    calculatedTime = '30:00'; // replace with your logic
+                    basePace = 2;
                     break;
                   default:
                     break;
                 }
 
-                setPace(selectedSpeed); // Update the pace state variable with the selected speed
-                setTime(calculatedTime);
+                // Convert distance to a number
+                let distanceNumber = parseFloat(distance);
+
+                // If distanceUnits is kilometers and basePace is in miles, convert basePace to kilometers
+                if (distanceUnits === 'kilometers') {
+                  basePace /= 1.60934; // 1 mile is approximately 1.60934 kilometers
+                }
+
+                // Calculate pace and time
+                let calculatedPace = basePace;
+                let calculatedTime = basePace * distanceNumber;
+
+                // Convert calculatedTime to HH:MM:SS format
+                let hours = Math.floor(calculatedTime / 60);
+                let minutes = Math.floor(calculatedTime % 60);
+                let seconds = Math.floor((calculatedTime * 60) % 60);
+
+                calculatedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+                // Convert calculatedPace to MM:SS format
+                minutes = Math.floor(calculatedPace);
+                seconds = Math.floor((calculatedPace * 60) % 60);
+
+                calculatedPace = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+                setPace(calculatedPace); // Update the pace state variable with the calculated pace
+                setTime(calculatedTime); // Update the time state variable with the calculated time
+
+
               }}
               className="p-2 border border-gray-300 rounded w-full"
             >
@@ -258,23 +254,36 @@ const Home = () => {
           <button type="submit" className={`p-2 ${imageRendering ? 'bg-green-700' : 'bg-blue-500 hover:bg-blue-600'} text-white rounded w-full items-center mx-auto`}>
             {imageRendering ? (
               <span className="flex mx-auto align-center items-center space-x-2 justify-center items-center">
-                <FontAwesomeIcon icon={faSpinner} spin />
+                <FontAwesomeIcon className='' icon={faSpinner} spin />
                 <span>Running...</span>
               </span>
             ) : (
-              "Generate run"
+              <span className="flex mx-auto align-center items-center space-x-1.5 justify-center items-center group">
+                <span>Generate run</span>
+                <FontAwesomeIcon icon={faAngleRight} className="w-3.5 h-3.5 transition-transform duration-100 group-hover:translate-x-0.5" />
+              </span>
             )}
           </button>
 
           <>
             {imageRendering ?
-              (<div className='font-light text-xs'>Did you know? AI runs take about 15 seconds to generate, but will save you hours of exertion.</div>) : ""
+              (<div className='font-light text-xs'>Did you know? AI runs take about 10 seconds to generate, but will save you hours of exertion.</div>) : ""
             }
           </>
         </form>
       </div>
 
-      <canvas ref={canvasRef} style={{ width: "560", height: "744" }} className="mt-2 pb-0"></canvas>
+      <canvas ref={canvasRef} style={{ width: 560, height: 744 }} className="mt-2 mb-1"></canvas>
+
+      <style jsx>{`
+        @media (max-width: 600px) {
+          canvas {
+            width: 100% !important;
+            height: auto !important;
+          }
+        }
+      `}</style>
+
 
       {imageRendered && (
         <button className='border rounded-md mt-2 bg-gray-200 hover:bg-gray-300 px-4 py-2 mb-8' onClick={() => {
