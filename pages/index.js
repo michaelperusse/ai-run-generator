806530bf5b7a46891cdfc40339eb8e9d83d520f1
Michael Perusse <michaelperusse@Michaels-MBP-2.localdomain>
2024-01-01 19:15:22 -0800

    strava parody logo

--- pages/index.js
+++ pages/index.js
@@ -20,129 +20,129 @@ const Home = () => {
   const canvasRef = useRef(null);
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  setImageRendering(true);
+    e.preventDefault();
+    setImageRendering(true);
 
-  const canvas = canvasRef.current;
-  const ctx = canvas.getContext('2d');
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
 
-  if (imageDescription === '') {
-    imageDescription = "central park NYC";
-  }
-  if (pace === '') {
-    pace = "5";
-  }
-  if (time === '') {
-    time = "5";
-  }
-  if (pace === '') {
-    pace = "5";
-  }
-
-  imageDescription = "photorealistic 4K photo of " + imageDescription;
-
-  try {
-    const response = await fetch('/api/predict', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ imageDescription }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    if (imageDescription === '') {
+      imageDescription = "central park NYC";
+    }
+    if (pace === '') {
+      pace = "5";
+    }
+    if (time === '') {
+      time = "5";
+    }
+    if (pace === '') {
+      pace = "5";
     }
 
-    let userImageLoaded = false;
-    let stravaLogoLoaded = false;
+    imageDescription = "photorealistic 4K photo of " + imageDescription;
 
-    let prediction = await response.json();
-    const userImage = new Image();
-    userImage.crossOrigin = "anonymous";
-    userImage.src =  prediction.output[0];
+    try {
+      const response = await fetch('/api/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ imageDescription }),
+      });
 
-    userImage.onload = () => {
-      userImageLoaded = true;
-      drawImages();
-    };
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-    const stravaLogo = new Image();
-    stravaLogo.crossOrigin = "anonymous";
-    stravaLogo.src = '/images/STRAVA-LOGO.png'; // Path to the Strava logo
-    stravaLogo.onload = () => {
-      stravaLogoLoaded = true;
-      drawImages();
-    };
+      let userImageLoaded = false;
+      let stravaLogoLoaded = false;
 
-    function drawImages() {
+      let prediction = await response.json();
+      const userImage = new Image();
+      userImage.crossOrigin = "anonymous";
+      userImage.src = prediction.output[0];
 
-      if (userImageLoaded && stravaLogoLoaded) {
-        ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
-        ctx.font = '16px Arial';
-  
-        const canvasWidth = canvas.width;
-  
-        const textTitlePosY = canvas.height - canvas.height / 10;
-        const subtitlePosY = textTitlePosY + canvas.height / 22;
-  
-        ctx.font = '18px Tahoma';
-        ctx.fillStyle = 'white';
-        ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
-        ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
+      userImage.onload = () => {
+        userImageLoaded = true;
+        drawImages();
+      };
+
+      const stravaLogo = new Image();
+      stravaLogo.crossOrigin = "anonymous";
+      stravaLogo.src = '/images/STRAVAI-LOGO.png';
+      stravaLogo.onload = () => {
+        stravaLogoLoaded = true;
+        drawImages();
+      };
+
+      function drawImages() {
+
+        if (userImageLoaded && stravaLogoLoaded) {
+          ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
+          ctx.font = '16px Arial';
+
+          const canvasWidth = canvas.width;
+
+          const textTitlePosY = canvas.height - canvas.height / 10;
+          const subtitlePosY = textTitlePosY + canvas.height / 22;
+
+          ctx.font = '18px Tahoma';
+          ctx.fillStyle = 'white';
+          ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
+          ctx.fillText(`Pace`, (canvasWidth / 3) * 1 + (canvasWidth / 10), textTitlePosY);
 
 
-        ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
-  
-        ctx.font = '30px Tahoma';
-        ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth  / 3) * 0 + (canvasWidth / 10), subtitlePosY);
-        ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
-        ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
-  
-        // stravaLogo.onload = () => {
+          ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10), textTitlePosY);
+
+          ctx.font = '30px Tahoma';
+          ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 0 + (canvasWidth / 10), subtitlePosY);
+          ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
+          ctx.fillText(`${time}`, (canvasWidth / 3) * 2 + (canvasWidth / 10), subtitlePosY);
+
+          // stravaLogo.onload = () => {
           const stravaRatio = 4.76;
           const stravaWidth = 100;
           const stravaHeight = stravaWidth / stravaRatio;
           ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
-        // };
+          // };
 
-        setImageRendering(false);
-        setImageRendered(true);
+          setImageRendering(false);
+          setImageRendered(true);
+        }
+
       }
 
+      // userImage.onload = () => {
+      //   ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
+      //   ctx.font = '16px Arial';
+
+      //   const canvasWidth = canvas.width;
+
+      //   const textTitlePosY = canvas.height - canvas.height / 10;
+      //   const subtitlePosY = textTitlePosY + canvas.height / 22;
+
+      //   ctx.font = '18px Tahoma';
+      //   ctx.fillStyle = 'white';
+      //   ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
+      //   ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
+      //   ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
+
+      //   ctx.font = '30px Tahoma';
+      //   ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth  / 3) * 0 + (canvasWidth / 10), subtitlePosY);
+      //   ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
+      //   ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
+
+      //   stravaLogo.onload = () => {
+      //     const stravaRatio = 4.76;
+      //     const stravaWidth = 100;
+      //     const stravaHeight = stravaWidth / stravaRatio;
+      //     ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
+      //   };
+      // };
+    } catch (error) {
+      console.error('Error creating prediction:', error);
     }
-
-    // userImage.onload = () => {
-    //   ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
-    //   ctx.font = '16px Arial';
-
-    //   const canvasWidth = canvas.width;
-
-    //   const textTitlePosY = canvas.height - canvas.height / 10;
-    //   const subtitlePosY = textTitlePosY + canvas.height / 22;
-
-    //   ctx.font = '18px Tahoma';
-    //   ctx.fillStyle = 'white';
-    //   ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
-    //   ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
-    //   ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
-
-    //   ctx.font = '30px Tahoma';
-    //   ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth  / 3) * 0 + (canvasWidth / 10), subtitlePosY);
-    //   ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
-    //   ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
-
-    //   stravaLogo.onload = () => {
-    //     const stravaRatio = 4.76;
-    //     const stravaWidth = 100;
-    //     const stravaHeight = stravaWidth / stravaRatio;
-    //     ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
-    //   };
-    // };
-  } catch (error) {
-    console.error('Error creating prediction:', error);
-  }
-};
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full pt-20">
@@ -160,7 +160,7 @@ const Home = () => {
               onChange={(e) => setDistance(e.target.value)}
               className="p-2 border border-gray-300 rounded w-full"
             />
-            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Distance</span>}
+            {<span className='bg-white text-xs' style={{ position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray" }}>Distance</span>}
           </label>
           <div className="flex items-center space-x-4">
             <label>
@@ -202,9 +202,9 @@ const Home = () => {
                     calculatedTime = '60:00'; // replace with your logic
                     break;
                   case 'respectable':
-                      calculatedPace = '10:00'; // replace with your logic
-                      calculatedTime = '60:00'; // replace with your logic
-                      break;
+                    calculatedPace = '10:00'; // replace with your logic
+                    calculatedTime = '60:00'; // replace with your logic
+                    break;
                   case 'fast':
                     calculatedPace = '7:30'; // replace with your logic
                     calculatedTime = '45:00'; // replace with your logic
@@ -214,9 +214,9 @@ const Home = () => {
                     calculatedTime = '30:00'; // replace with your logic
                     break;
                   case 'unbelievablyfast':
-                      calculatedPace = '5:00'; // replace with your logic
-                      calculatedTime = '30:00'; // replace with your logic
-                      break;
+                    calculatedPace = '5:00'; // replace with your logic
+                    calculatedTime = '30:00'; // replace with your logic
+                    break;
                   default:
                     break;
                 }
@@ -232,10 +232,10 @@ const Home = () => {
               <option value="fast">🐎 Fast</option>
               <option value="wickedfast">🐆 Wicked Fast</option>
               <option value="unbelievablyfast">🚀 Unbelievably Fast</option>
-              
+
             </select>
 
-            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Pace</span>}
+            {<span className='bg-white text-xs' style={{ position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray" }}>Pace</span>}
 
           </label>
 
@@ -248,49 +248,48 @@ const Home = () => {
               placeholder="Central Park NYC"
               className="p-2 border border-gray-300 rounded w-full"
             />
-            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Image description</span>}
+            {<span className='bg-white text-xs' style={{ position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray" }}>Run location</span>}
           </label>
 
-            
 
 
-            
-            <button type="submit" className={`p-2 ${imageRendering ? 'bg-green-700' : 'bg-blue-500 hover:bg-blue-600'} text-white rounded w-full items-center mx-auto`}>
-              {imageRendering ? (
-                <span className="flex mx-auto align-center items-center space-x-2 justify-center items-center">
-                  <FontAwesomeIcon icon={faSpinner} spin />
-                  <span>Running...</span>
-                </span>
-              ) : (
-                "Generate run"
-              )}
-            </button>
 
-            <>
-            { imageRendering ?
+
+          <button type="submit" className={`p-2 ${imageRendering ? 'bg-green-700' : 'bg-blue-500 hover:bg-blue-600'} text-white rounded w-full items-center mx-auto`}>
+            {imageRendering ? (
+              <span className="flex mx-auto align-center items-center space-x-2 justify-center items-center">
+                <FontAwesomeIcon icon={faSpinner} spin />
+                <span>Running...</span>
+              </span>
+            ) : (
+              "Generate run"
+            )}
+          </button>
+
+          <>
+            {imageRendering ?
               (<div className='font-light text-xs'>Did you know? AI runs take about 15 seconds to generate, but will save you hours of exertion.</div>) : ""
             }
-            </>
-          </form>
-          </div>
-
-          <canvas ref={canvasRef} width="560" height="744" className="mt-8 pb-20"></canvas>
-          
-
-          {imageRendered && (
-            <button className='border rounded-md mt-2 bg-gray-200 px-4 p-2' onClick={() => {
-              let link = document.createElement('a');
-              link.download = 'generated_run.png';
-              link.href = canvasRef.current.toDataURL()
-              link.click();
-            }}>
-              Download Image
-            </button>
-          )}
+          </>
+        </form>
       </div>
 
-      
-    );
-  };
-  
-  export default Home;
+      <canvas ref={canvasRef} width="560" height="744" className="mt-2 pb-0"></canvas>
+
+      {imageRendered && (
+        <button className='border rounded-md mt-2 bg-gray-200 hover:bg-gray-300 px-4 py-2 mb-8' onClick={() => {
+          let link = document.createElement('a');
+          link.download = 'generated_run.png';
+          link.href = canvasRef.current.toDataURL()
+          link.click();
+        }}>
+          Download image
+        </button>
+      )}
+    </div>
+
+
+  );
+};
+
+export default Home;
