ff116c17cf6de2ec40f90389191f413536e800a3
Michael Perusse <michaelperusse@Michaels-MBP-2.localdomain>
2024-01-01 18:51:30 -0800

    form updates, API updates

--- pages/api/predict.js
+++ pages/api/predict.js
@@ -1,4 +1,3 @@
-// pages/api/predict.js
 import Replicate from "replicate";
 
 const replicate = new Replicate({
@@ -8,19 +7,27 @@ const replicate = new Replicate({
 export default async (req, res) => {
   try {
     const { imageDescription } = req.body;
-    let prediction = await replicate.deployments.predictions.create(
-      "michaelperusse",
-      "fake-strava-run",
+    let prediction = await replicate.predictions.create(
       {
+        version: "39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
         input: {
           prompt: imageDescription,
-          width: 1120,
-          height: 1488,
+          width: 1120 / 2,
+          height: 1488 / 2,
         }
       }
     );
+
+    console.log(prediction);
+
+    // Use Replicate's built-in wait method
+    prediction = await replicate.wait(prediction);
+
+    console.log(prediction.output);
+
     res.status(200).json(prediction);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.toString() });
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,82 @@
 import React, { useState, useRef } from 'react';
 import '../src/app/globals.css';
 import Replicate from "replicate";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
 const Home = () => {
-  const [distance, setDistance] = useState('');
-  const [distanceUnits, setDistanceUnits] = useState('');
-  const [pace, setPace] = useState('');
-  const [time, setTime] = useState('');
-  const [imageDescription, setImageDescription] = useState('');
+  let [distance, setDistance] = useState('');
+  let [distanceUnits, setDistanceUnits] = useState('');
+  let [pace, setPace] = useState('');
+  let [time, setTime] = useState('');
+  let [imageDescription, setImageDescription] = useState('');
+  let [imageRendering, setImageRendering] = useState(false);
+  let [imageRendered, setImageRendered] = useState(false);
   const canvasRef = useRef(null);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-  
-    try {
-      const response = await fetch('/api/predict', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ imageDescription }),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      let prediction = await response.json();
-      const userImage = new Image();
-      userImage.src = prediction.output.url;
-  
-      const stravaLogo = new Image();
-      stravaLogo.src = '/images/STRAVA-LOGO.png'; // Path to the Strava logo
-  
-      userImage.onload = () => {
+  e.preventDefault();
+  setImageRendering(true);
+
+  const canvas = canvasRef.current;
+  const ctx = canvas.getContext('2d');
+
+  if (imageDescription === '') {
+    imageDescription = "central park NYC";
+  }
+  if (pace === '') {
+    pace = "5";
+  }
+  if (time === '') {
+    time = "5";
+  }
+  if (pace === '') {
+    pace = "5";
+  }
+
+  imageDescription = "photorealistic 4K photo of " + imageDescription;
+
+  try {
+    const response = await fetch('/api/predict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ imageDescription }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    let userImageLoaded = false;
+    let stravaLogoLoaded = false;
+
+    let prediction = await response.json();
+    const userImage = new Image();
+    userImage.crossOrigin = "anonymous";
+    userImage.src =  prediction.output[0];
+
+    userImage.onload = () => {
+      userImageLoaded = true;
+      drawImages();
+    };
+
+    const stravaLogo = new Image();
+    stravaLogo.crossOrigin = "anonymous";
+    stravaLogo.src = '/images/STRAVA-LOGO.png'; // Path to the Strava logo
+    stravaLogo.onload = () => {
+      stravaLogoLoaded = true;
+      drawImages();
+    };
+
+    function drawImages() {
+
+      if (userImageLoaded && stravaLogoLoaded) {
         ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
         ctx.font = '16px Arial';
   
@@ -54,6 +90,8 @@ const Home = () => {
         ctx.fillStyle = 'white';
         ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
         ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
+
+
         ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
   
         ctx.font = '30px Tahoma';
@@ -61,31 +99,69 @@ const Home = () => {
         ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
         ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
   
-        stravaLogo.onload = () => {
+        // stravaLogo.onload = () => {
           const stravaRatio = 4.76;
           const stravaWidth = 100;
           const stravaHeight = stravaWidth / stravaRatio;
           ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
-        };
-      };
-    } catch (error) {
-      console.error('Error creating prediction:', error);
+        // };
+
+        setImageRendering(false);
+        setImageRendered(true);
+      }
+
     }
-  };
+
+    // userImage.onload = () => {
+    //   ctx.drawImage(userImage, 0, 0, canvas.width, canvas.height);
+    //   ctx.font = '16px Arial';
+
+    //   const canvasWidth = canvas.width;
+
+    //   const textTitlePosY = canvas.height - canvas.height / 10;
+    //   const subtitlePosY = textTitlePosY + canvas.height / 22;
+
+    //   ctx.font = '18px Tahoma';
+    //   ctx.fillStyle = 'white';
+    //   ctx.fillText(`Run`, (canvasWidth / 3) * 0 + (canvasWidth / 10), textTitlePosY);
+    //   ctx.fillText(`Pace`, (canvasWidth  / 3) * 1 + (canvasWidth / 10), textTitlePosY);
+    //   ctx.fillText(`Time`, (canvasWidth / 3) * 2 + (canvasWidth / 10) , textTitlePosY);
+
+    //   ctx.font = '30px Tahoma';
+    //   ctx.fillText(`${distance} ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth  / 3) * 0 + (canvasWidth / 10), subtitlePosY);
+    //   ctx.fillText(`${pace} / ${distanceUnits === 'kilometers' ? 'km' : 'mi'}`, (canvasWidth / 3) * 1 + (canvasWidth / 10), subtitlePosY);
+    //   ctx.fillText(`${time}`, (canvasWidth  / 3) * 2 + (canvasWidth / 10), subtitlePosY);
+
+    //   stravaLogo.onload = () => {
+    //     const stravaRatio = 4.76;
+    //     const stravaWidth = 100;
+    //     const stravaHeight = stravaWidth / stravaRatio;
+    //     ctx.drawImage(stravaLogo, canvasWidth / 30, canvasWidth / 30, stravaWidth, stravaHeight);
+    //   };
+    // };
+  } catch (error) {
+    console.error('Error creating prediction:', error);
+  }
+};
 
   return (
-    <div className="flex flex-col items-center mx-auto justify-center min-h-screen pt-20">
-      <div className="w-full max-w-md mx-auto">
-        <h1 className="text-4xl text-gray-800 font-bold mb-6 text-center">AI Strava Run Generator</h1>
+    <div className="flex flex-col items-center mx-auto justify-center min-h-screen w-full pt-20 flex items-center justify-center">
+      <div className="w-full max-w-md mx-auto border p-12 rounded-xl shadow-sm">
+        <h1 className="text-4xl text-gray-800 font-bold mb-2 text-left">AI Run Generator 🏃‍♀️</h1>
 
-        <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-8 mx-auto justify-center">
-          <input
-            type="number"
-            placeholder="Distance (e.g. 6.32)"
-            value={distance}
-            onChange={(e) => setDistance(e.target.value)}
-            className="p-2 border border-gray-300 rounded w-full"
-          />
+        <div className='w-7/8 flex mx-auto align-center items-center border-b border-gray-200 mb-6'></div>
+
+        <form onSubmit={handleSubmit} className="flex flex-col items-left space-y-6 mx-auto justify-center">
+          <label className="relative">
+            <input
+              type="number"
+              placeholder="6.32"
+              value={distance}
+              onChange={(e) => setDistance(e.target.value)}
+              className="p-2 border border-gray-300 rounded w-full"
+            />
+            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Distance</span>}
+          </label>
           <div className="flex items-center space-x-4">
             <label>
               <input
@@ -106,35 +182,98 @@ const Home = () => {
               Kilometers
             </label>
           </div>
-          <input
-            type="text"
-            placeholder="Pace (e.g. 7:30)"
-            value={pace}
-            onChange={(e) => setPace(e.target.value)}
-            className="p-2 border border-gray-300 rounded w-full"
-            />
-            <input
-              type="text"
-              placeholder="Time (e.g. 48:30)"
-              value={time}
-              onChange={(e) => setTime(e.target.value)}
+
+          <label className='relative'>
+            <select
+              value={pace}
+              onChange={(e) => {
+                const selectedSpeed = e.target.value;
+                let calculatedPace, calculatedTime;
+
+                // Do all calculations in miles, update pace at the end if in km
+
+                switch (selectedSpeed) {
+                  case 'slow':
+                    calculatedPace = '10:00'; // replace with your logic
+                    calculatedTime = '60:00'; // replace with your logic
+                    break;
+                  case 'fast':
+                    calculatedPace = '7:30'; // replace with your logic
+                    calculatedTime = '45:00'; // replace with your logic
+                    break;
+                  case 'wicked fast':
+                    calculatedPace = '5:00'; // replace with your logic
+                    calculatedTime = '30:00'; // replace with your logic
+                    break;
+                  default:
+                    break;
+                }
+
+                setPace(selectedSpeed); // Update the pace state variable with the selected speed
+                setTime(calculatedTime);
+              }}
               className="p-2 border border-gray-300 rounded w-full"
-            />
+            >
+              <option value="slow">Slow</option>
+              <option value="fast">Fast</option>
+              <option value="wicked fast">Wicked Fast</option>
+            </select>
+
+            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Pace</span>}
+
+          </label>
+
+
+          <label className="relative">
             <input
               type="text"
-              placeholder="Image Description (e.g. An astronaut riding a rainbow unicorn)"
               value={imageDescription}
               onChange={(e) => setImageDescription(e.target.value)}
+              placeholder="Central Park NYC"
               className="p-2 border border-gray-300 rounded w-full"
             />
-            <button type="submit" className="p-2 bg-blue-500 text-white rounded w-full">
-              Generate
+            {<span className='bg-white text-xs' style={{position: "absolute", top: "-12px", left: "0", padding: "2px", color: "gray"}}>Image description</span>}
+          </label>
+
+            
+
+
+            
+            <button type="submit" className={`p-2 ${imageRendering ? 'bg-green-700' : 'bg-blue-500 hover:bg-blue-600'} text-white rounded w-full items-center mx-auto`}>
+              {imageRendering ? (
+                <span className="flex mx-auto align-center items-center space-x-2 justify-center items-center">
+                  <FontAwesomeIcon icon={faSpinner} spin />
+                  <span>Running...</span>
+                </span>
+              ) : (
+                "Generate run"
+              )}
             </button>
+
+            <>
+            { imageRendering ?
+              (<div className='font-light text-xs'>Did you know? AI runs take about 15-30 seconds to generate, but will save you hours of exertion.</div>) : ""
+            }
+            </>
           </form>
-  
-          <canvas ref={canvasRef} width="1120" height="1488" className="mt-8"></canvas>
-        </div>
+          </div>
+
+          {imageRendered && (
+            <button className='border rounded-md mt-2 bg-gray-200 px-4 p-2' onClick={() => {
+              let link = document.createElement('a');
+              link.download = 'generated_run.png';
+              link.href = canvasRef.current.toDataURL()
+              link.click();
+            }}>
+              Download Image
+            </button>
+          )}
+          <canvas ref={canvasRef} width="560" height="744" className="mt-8 pb-20"></canvas>
+          
+
       </div>
+
+      
     );
   };
   
